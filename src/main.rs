bc60d2daa641b51ddd7c648b4336f6e4a4bb990a
welingtond <welingtonpdias@gmail.com>
2025-03-06 14:01:09 -0300

    Final version

--- src/main.rs
+++ src/main.rs
@@ -4,52 +4,38 @@
 
 #[derive(Debug)]
 struct Sizes {
-    bytes: u64,
-    kilobytes: f64,
-    megabytes: f64,
-    gigabytes: f64,
-    terabytes: f64,
+    bytes: String,
+    kilobytes: String,
+    megabytes: String,
+    gigabytes: String,
+    terabytes: String,
 }
 
 impl Sizes {
     fn new(bytes: String) -> Self {
-        let bytes = bytes.trim().split(" ").collect::<Vec<&str>>();
-        let bytes = bytes[0].parse::<u64>().unwrap();
-        Sizes {
-            bytes,
-            kilobytes: bytes as f64 * 1024.0,
-            megabytes: bytes as f64 * 1_024_000.0,
-            gigabytes: bytes as f64 * 1_024_000_000.0,
-            terabytes: bytes as f64 * 1_024_000_000_000.0,
-        }
-    }
-}
-enum FileSize {
-    Bytes(u64),
-    Kilobytes(f64),
-    Megabytes(f64),
-    Gigabytes(f64),
-    Terabytes(f64),
-}
-
-impl FileSize {
-
-    fn translate(linha: String) -> Self {
-        let mut linha = linha.trim().split_whitespace();
-        let size = linha.next().unwrap().parse::<u64>().unwrap();
-        let unit = linha.next().unwrap();
-
-        match unit {
-            "B" => FileSize::Bytes(size),
-            "KB" => FileSize::Kilobytes(size as f64),
-            "MB" => FileSize::Megabytes(size as f64),
-            "GB" => FileSize::Gigabytes(size as f64),
-            "TB" => FileSize::Terabytes(size as f64),
+        let mut literal = bytes.trim().split(" ").collect::<Vec<&str>>();
+        let bytes = literal[0].parse::<u64>().unwrap();
+        let descr: &str = literal[1];
+        let bytes = match descr {
+            "b" | "B" => bytes,
+            "kb" | "KB" => bytes * 1024,
+            "mb" | "MB" => bytes * 1_024_000,
+            "gb" | "GB" => bytes * 1_024_000_000,
+            "tb" | "TB" => bytes * 1_024_000_000_000,
             _ => panic!("Invalid unit"),
+        };
+
+        Sizes {
+            bytes: format!("{}  bytes", bytes),
+            kilobytes: format!("{:.2} kilobytes", bytes as f64 / 1024.0),
+            megabytes: format!("{:.2} megabytes", bytes as f64 / 1_024_000.0),
+            gigabytes: format!("{:.2} gigabytes", bytes as f64 / 1_024_000_000.0),
+            terabytes: format!("{:.2} terabytes", bytes as f64 / 1_024_000_000_000.0),
         }
     }
+}
 
-    fn format_size(size: u64) -> String {
+/*     fn format_size(size: u64) -> String {
         let filesize = match size {
             0..=1023 => FileSize::Bytes(size),
             1024..=1_023_999 => FileSize::Kilobytes(size as f64 / 1024.0),
@@ -66,7 +52,7 @@ impl FileSize {
             FileSize::Terabytes(tb) => format!("{:.2} TB", tb),
         }
     }
-}
+ */
 
 
 fn main() {
